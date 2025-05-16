8aeb0e6a4fda5a11f8044cecaaa90daabc4c2dc4
Marcos García <123456789+marcos.g.costell@outlook.com>
2025-05-16 23:42:25 +0200

    riot-api-converted-to-class

--- src/modules/main.js
+++ src/modules/main.js
@@ -1,12 +1,9 @@
-'use strict';
-
 ///////////////////////////////////////
 // LOL Drafting tool
 
 //Importing from modules
-// import * as Lolalytics from './lolalytics-api.js';
-import { Lolalytics } from './lolalytics-class-api.js';
-import { getRiotLastVersion, updateDataFromRiot } from './riot-api.js';
+import Lolalytics from './lolalytics-api.js';
+import Riot from './riot-api.js';
 import {
   getLocalVersion,
   getLocalChampions,
@@ -35,16 +32,16 @@ async function init() {
   // Checks if updating the champion info is necesary
   // and gets the champion info
   const storedVersion = await getLocalVersion();
-  const version = await getRiotLastVersion();
+  const version = await Riot.getLastVersion();
 
   if (version === storedVersion) {
     champions = await getLocalChampions();
   } else {
-    champions = await updateDataFromRiot();
+    champions = await Riot.updateDataFromServer();
     // saveChampionsData(champions, version);
   }
   console.log(champions);
-  console.log(champions.Aatrox);
+  console.log(champions['Aatrox']);
 
   // Get the champion names list
   const championList = getChampionsList(champions);
@@ -64,20 +61,11 @@ async function init() {
   }
 
   return lolalytics;
-  // const lolayticsUrls = new Map(
-  //   await lolalytics.getChampionFolders(championList)
-  // );
-  // console.log(
-  //   lolayticsUrls.get('integrity')
-  //     ? 'Path list is OK'
-  //     : 'There is an error on the champion list'
-  // );
-  // console.log(lolayticsUrls);
 }
 
 const test = async function (lolalytics) {
   await wait(getRandomNumber(0.7, 2.2));
-  const counters = await lolalytics.getCounters('lux', 'all', 'middle');
+  const counters = await lolalytics.getCounters('Lux', 'all', 'middle');
   console.log(counters);
 
   await wait(getRandomNumber(0.7, 2.2));
--- src/modules/riot-api.js
+++ src/modules/riot-api.js
@@ -1,82 +1,102 @@
-'use strict';
-// import * as fs from 'node:fs';
-
 ///////////////////////////////////////
-// Global variables
-const locale = 'en_US';
-const dataDragon = 'https://ddragon.leagueoflegends.com/';
-
-///////////////////////////////////////
-// Private module functions
-
+// Riot Class
 /**
- * @async
- * @function getRiotChampionData
- * Gets the League of Legends' champion data from Riot API
- * @param {String} version Version patch of the data.
- * @return {Promise<Object>} Object where every champion is a property.
+ *
+ * @class Riot -
+ * API to get data from Riot Servers
+ * @property {String} dataDragon (#) base url of the website.
+ * @property {Boolean} locale (#) locale language settings.
+ * @property {Map} championFolders Map key='Champion name' value='folder'.
+ * @property {Boolean} listIntegrity true if all Champion names in the map are ok.
+ *
+ * PUBLIC METHODS
+ * @method #getChampionData Get champions complete data from Riot servers
+ * @method getLastVersion Get last verion from Riot servers
+ * @method updateDataFromServer Returns an array of objects with selected champion data
  */
-const getRiotChampionData = async function (version) {
-  const url = `${dataDragon}cdn/${version}/data/${locale}/champion.json`;
 
-  try {
-    const response = await fetch(url);
-    const lolChampions = await response.json();
+export default class Riot {
+  static #dataDragon = 'https://ddragon.leagueoflegends.com/';
+  static #locale = 'en_US';
 
-    return lolChampions.data;
-  } catch (err) {
-    console.error(err);
+  constructor() {}
+
+  // PRIVATE METHODS
+
+  /**
+   * @async @method #getChampionData
+   * Gets the League of Legends' champion data from Riot API
+   * @param {String} version Version patch of the data.
+   * @return {Promise<Object>} Object where every champion is a property.
+   */
+  static async #getChampionData(version) {
+    const url = `${this.#dataDragon}cdn/${version}/data/${
+      this.#locale
+    }/champion.json`;
+
+    try {
+      const response = await fetch(url);
+      const lolChampions = await response.json();
+
+      return lolChampions.data;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
   }
-};
 
-///////////////////////////////////////
-// Exported functions
+  // PUBLIC METHODS
 
-/**
- * @async
- * @function getRiotLastVersion
- * Gets the League of Legends' last version from Riot API
- * @return {Promise<String>} The last version.
- */
-export const getRiotLastVersion = async function () {
-  const url = `${dataDragon}api/versions.json`;
+  /**
+   * @async @method getLastVersion
+   * Gets the League of Legends' last version from Riot API
+   * @return {Promise<String>} The last version.
+   */
+  static async getLastVersion() {
+    const url = `${this.#dataDragon}api/versions.json`;
 
-  try {
-    const response = await fetch(url);
-    const [data] = await response.json();
-    return data;
-  } catch (err) {
-    console.error(err);
+    try {
+      const response = await fetch(url);
+      const [data] = await response.json();
+      return data;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
   }
-};
 
-/**
- * @async
- * @function updateDataFromRiot
- * Read updated version of champions' data from Riot API
- * and create an object with a reduced info version of all the champions
- * Calls to save data
- * @param {String} version Version patch of the data. No arg, get the last version
- * @return {Promise<Object>} Object where every champion is a property.
- */
-export const updateDataFromRiot = async function (version = null) {
-  // No verion passed, gets the last version
-  if (!version) version = await getRiotLastVersion();
+  /**
+   * @async @method updateDataFromServer
+   * Read updated version of champions' data from Riot API
+   * and create an object with a reduced info version of all the champions
+   * Calls to save data
+   * @param {String} version Version patch of the data. No arg, get the last version
+   * @return {Promise<Object>} Object where every champion is a property.
+   */
+  static async updateDataFromServer(version = null) {
+    // No version passed, gets the last version
+    try {
+      if (!version) version = await this.getLastVersion();
 
-  const championsData = {};
-  const lolChampions = await getRiotChampionData(version);
+      const lolChampions = await this.#getChampionData(version);
+      const championsData = {};
 
-  // In championsData create an object for each champion with some data
-  Object.keys(lolChampions).forEach(
-    championName =>
-      (championsData[championName] = {
-        version: lolChampions[championName].version,
-        id: lolChampions[championName].id,
-        key: lolChampions[championName].key,
-        name: lolChampions[championName].name,
-        img: lolChampions[championName].image.full,
-      })
-  );
+      // In championsData create an object for each champion with some data
+      Object.keys(lolChampions).forEach(
+        championName =>
+          (championsData[championName] = {
+            version: lolChampions[championName].version,
+            id: lolChampions[championName].id,
+            key: lolChampions[championName].key,
+            name: lolChampions[championName].name,
+            img: lolChampions[championName].image.full,
+          })
+      );
 
-  return championsData;
-};
+      return championsData;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  }
+}
